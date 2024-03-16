724a975950033160877c94fa607f70a839de5cb3
Porobeam <fzgbeam@gmail.com>
2024-03-16 03:00:16 -0600

    video background moved pinned to follow

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -4,7 +4,7 @@ import HeroContent from "../sub/HeroContent";
 const Hero = () => {
   return (
     <div className="relative flex flex-col w-full h-screen" id="about-me">
-      <video autoPlay muted loop className="absolute top-0 left-0 min-w-full h-full z-0 object-cover">
+      <video autoPlay muted loop className="fixed top-0 left-0 min-w-full h-full z-0 object-cover opacity-80">
         <source src="/world.mp4" type="video/mp4" />
       </video>
       <HeroContent />
